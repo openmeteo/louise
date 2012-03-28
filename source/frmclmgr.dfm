44f836a5c5cebe979de55c6103f24f5e92b8d275
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2012-03-29 00:32:36 +0300

    Working on Climacograms, improve algorithm, first graph, ticket #256

--- source/frmclmgr.dfm
+++ source/frmclmgr.dfm
@@ -12,15 +12,33 @@ object FrmClimacogram: TFrmClimacogram
   Font.Style = []
   OldCreateOrder = False
   OnCreate = FormCreate
+  OnShow = FormShow
+  DesignSize = (
+    518
+    410)
   PixelsPerInch = 96
   TextHeight = 13
   object Chart: TChart
-    Left = 8
-    Top = 8
-    Width = 425
-    Height = 313
+    Left = 0
+    Top = 0
+    Width = 521
+    Height = 409
+    MarginTop = 5
     Title.Text.Strings = (
       'TChart')
+    BottomAxis.Logarithmic = True
+    BottomAxis.MinorGrid.Color = clSilver
+    BottomAxis.MinorGrid.Visible = True
+    BottomAxis.MinorTickCount = 8
+    LeftAxis.Logarithmic = True
+    LeftAxis.MinorGrid.Color = clSilver
+    LeftAxis.MinorGrid.Visible = True
+    LeftAxis.MinorTickCount = 8
+    LeftAxis.PositionUnits = muPixels
+    View3D = False
+    View3DOptions.Orthogonal = False
+    Color = clWhite
     TabOrder = 0
+    Anchors = [akLeft, akTop, akRight, akBottom]
   end
 end
--- source/frmclmgr.pas
+++ source/frmclmgr.pas
@@ -28,16 +28,19 @@ const
      (mult:1/24; desc:'hour'), (mult:1; desc:'Day'),
      (mult:7; desc:'week'), (mult:30.44; desc:'month'),
      (mult:365.25; desc:'year'), (mult:3652.5; desc:'ten years'),
-     (mult:36252.5; desc:'century'), (mult:362525; desc:'millenium'));
+     (mult:36525; desc:'century'), (mult:3625250; desc:'millenium'));
 
 type
   TFrmClimacogram = class(TForm)
     Chart: TChart;
     procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     FTimeseriesGrid: TTimeseriesGrid;
     procedure SetTimeseriesGrid(AValue: TTimeseriesGrid);
     procedure PrepareLineSeries(Timeseries: TTimeseries; Series: TLineSeries;
+                                all: Boolean);
+    procedure AddLineSeries(Timeseries: TTimeseries; Chart: TChart;
                                 all: Boolean);
   public
     property TimeseriesGrid: TTimeseriesGrid read FTimeseriesGrid
@@ -48,11 +51,17 @@ implementation
 
 {$R *.dfm}
 
-uses climacgr;
+uses climacgr, math;
 
 procedure TFrmClimacogram.FormCreate(Sender: TObject);
 begin
   FTimeseriesGrid := nil;
+end;
+
+procedure TFrmClimacogram.FormShow(Sender: TObject);
+begin
+  AddLineSeries(TimeseriesGrid.Data[0], Chart, False);
+  AddLineSeries(TimeseriesGrid.Data[0], Chart, True);
 end;
 
 procedure TFrmClimacogram.SetTimeseriesGrid(AValue: TTimeseriesGrid);
@@ -66,29 +75,54 @@ type
 const
   scaleFunctions: array[False..True] of TScaleFunc = (AggrStDev, AggrStDevAll);
 
+  factors: array[0..15] of Integer = (1, 2, 4, 6, 8, 10, 12, 16, 20, 24, 30,
+                                      36, 48, 60, 80, 100);
+
 procedure TFrmClimacogram.PrepareLineSeries(Timeseries: TTimeseries;
                                             Series: TLineSeries; all: Boolean);
 var
   scaleFunction: TScaleFunc;
   maxScale: Integer;
   i: Integer;
+  fac: Integer;
   x, y, baseScale: Real;
 begin
   scaleFunction := scaleFunctions[all];
-  maxScale := Timeseries.Count div 10;
-  i := 1;
+  maxScale := Timeseries.Count div 5;
   if Timeseries.TimeStep.LengthMinutes>0 then
     baseScale := Timeseries.TimeStep.LengthMinutes/1440
   else if Timeseries.TimeStep.LengthMonths>0 then
     baseScale := Timeseries.TimeStep.LengthMonths*30.44
   else
     Assert(False);
-  while i<=maxScale do
+  i := 0;
+  fac := factors[0];
+  while fac<=maxScale do
   begin
-    x := baseScale * i;
-    y := scaleFunction(Timeseries, i);
+    x := baseScale * fac;
+    y := scaleFunction(Timeseries, fac);
     Series.AddXY(x, y);
-    i := i*2;
+    Inc(i);
+    if i<16 then
+      fac := factors[i]
+    else
+      fac := Trunc(IntPower(2, i-15))*100;
+  end;
+end;
+
+procedure TFrmClimacogram.AddLineSeries(Timeseries: TTimeseries; Chart: TChart;
+                                all: Boolean);
+var
+  ASeries: TLineSeries;
+begin
+  ASeries := nil;
+  try
+    ASeries := TLineSeries.Create(Chart);
+    PrepareLineSeries(Timeseries, ASeries, all);
+    Chart.AddSeries(ASeries);
+    ASeries := nil;
+  finally
+    ASeries.Free;
   end;
 end;
 
