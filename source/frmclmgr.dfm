4b8672cdfc8ff14c8667d8e82e6df85a07488eef
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2012-03-29 22:40:04 +0300

    Working on Climacograms, improve alot GUI, ticket #256

--- source/frmclmgr.dfm
+++ source/frmclmgr.dfm
@@ -1,44 +1,161 @@
 object FrmClimacogram: TFrmClimacogram
   Left = 0
   Top = 0
-  Caption = 'FrmClimacogram'
-  ClientHeight = 410
-  ClientWidth = 518
+  Caption = 'Climacogram of time series'
+  ClientHeight = 465
+  ClientWidth = 641
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  Menu = MainMenu
   OldCreateOrder = False
+  Position = poDesktopCenter
   OnCreate = FormCreate
   OnShow = FormShow
   DesignSize = (
-    518
-    410)
+    641
+    465)
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 493
+    Top = 8
+    Width = 128
+    Height = 52
+    Anchors = [akTop, akRight]
+    Caption = 
+      'Select one or multiple time series to display by clicking, dragg' +
+      'ing and / or using Ctrl and Shift keys:'
+    WordWrap = True
+    ExplicitLeft = 495
+  end
   object Chart: TChart
-    Left = 0
-    Top = 0
-    Width = 521
-    Height = 409
+    Left = 8
+    Top = 8
+    Width = 479
+    Height = 449
+    Legend.Alignment = laTop
+    Legend.LegendStyle = lsSeries
     MarginTop = 5
     Title.Text.Strings = (
       'TChart')
+    Title.Visible = False
     BottomAxis.Logarithmic = True
     BottomAxis.MinorGrid.Color = clSilver
     BottomAxis.MinorGrid.Visible = True
     BottomAxis.MinorTickCount = 8
+    BottomAxis.Title.Caption = 'scale (days)'
+    DepthAxis.Automatic = False
+    DepthAxis.AutomaticMaximum = False
+    DepthAxis.AutomaticMinimum = False
+    DepthAxis.Maximum = 0.319999999999999800
+    DepthAxis.Minimum = -0.680000000000000200
+    DepthTopAxis.Automatic = False
+    DepthTopAxis.AutomaticMaximum = False
+    DepthTopAxis.AutomaticMinimum = False
+    DepthTopAxis.Maximum = 0.319999999999999800
+    DepthTopAxis.Minimum = -0.680000000000000200
     LeftAxis.Logarithmic = True
     LeftAxis.MinorGrid.Color = clSilver
     LeftAxis.MinorGrid.Visible = True
     LeftAxis.MinorTickCount = 8
     LeftAxis.PositionUnits = muPixels
+    LeftAxis.Title.Caption = 'stdev'
+    RightAxis.Automatic = False
+    RightAxis.AutomaticMaximum = False
+    RightAxis.AutomaticMinimum = False
+    TopAxis.Automatic = False
+    TopAxis.AutomaticMaximum = False
+    TopAxis.AutomaticMinimum = False
+    TopAxis.Grid.Color = 16744576
+    TopAxis.Grid.Style = psDashDot
+    TopAxis.LabelStyle = talText
+    TopAxis.Logarithmic = True
     View3D = False
     View3DOptions.Orthogonal = False
+    BevelOuter = bvNone
     Color = clWhite
     TabOrder = 0
     Anchors = [akLeft, akTop, akRight, akBottom]
+  end
+  object lstSeries: TListBox
+    Left = 493
+    Top = 66
+    Width = 146
+    Height = 391
+    Anchors = [akTop, akRight, akBottom]
+    ItemHeight = 13
+    MultiSelect = True
+    TabOrder = 1
+    OnClick = lstSeriesClick
+  end
+  object MainMenu: TMainMenu
+    Left = 472
+    Top = 64
+    object File1: TMenuItem
+      Caption = 'File'
+      object mnuPrintChart: TMenuItem
+        Caption = 'Print chart'
+        ShortCut = 16464
+        OnClick = mnuPrintChartClick
+      end
+    end
+    object Edit1: TMenuItem
+      Caption = 'Edit'
+      object mnuCopyChart: TMenuItem
+        Caption = 'Copy chart'
+        ShortCut = 16451
+        OnClick = mnuCopyChartClick
+      end
+      object mnuCopyTabularData: TMenuItem
+        Caption = 'Copy tabular data'
+        ShortCut = 24643
+      end
+    end
+    object Methods1: TMenuItem
+      Caption = 'Method'
+      object First1: TMenuItem
+        Caption = 'Simple - single pass'
+        Checked = True
+        RadioItem = True
+        OnClick = First1Click
+      end
+      object Second1: TMenuItem
+        Caption = 'Multi pass'
+        RadioItem = True
+        OnClick = First1Click
+      end
+      object Both1: TMenuItem
+        Caption = 'Both'
+        RadioItem = True
+        OnClick = First1Click
+      end
+    end
+    object Span1: TMenuItem
+      Caption = 'Span'
+      object mnuOneToTen: TMenuItem
+        Caption = '1/10 (default)'
+        Checked = True
+        RadioItem = True
+        OnClick = mnuOneToTenClick
+      end
+      object mnuOneToFive: TMenuItem
+        Caption = '1/5'
+        RadioItem = True
+        OnClick = mnuOneToTenClick
+      end
+      object mnuOneToTwo: TMenuItem
+        Caption = '1/2'
+        RadioItem = True
+        OnClick = mnuOneToTenClick
+      end
+    end
+  end
+  object PrintDialog1: TPrintDialog
+    Left = 472
+    Top = 120
   end
 end
--- source/frmclmgr.pas
+++ source/frmclmgr.pas
@@ -14,7 +14,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, tsgrid, ts, ExtCtrls, TeeProcs, TeEngine, Chart, Series;
+  Dialogs, tsgrid, ts, ExtCtrls, TeeProcs, TeEngine, Chart, Series, StdCtrls,
+  Menus;
 
 type
   TTimeScale = record
@@ -23,25 +24,52 @@ type
   end;
 
 const
-   scalePoints:  array[0..10] of TTimeScale = ((mult:1/1440; desc:'minute'),
+   scalePoints:  array[0..12] of TTimeScale = ((mult:1/1440; desc:'minute'),
      (mult:1/288; desc:'five min.'), (mult:1/144; desc:'ten min.'),
      (mult:1/24; desc:'hour'), (mult:1; desc:'Day'),
-     (mult:7; desc:'week'), (mult:30.44; desc:'month'),
-     (mult:365.25; desc:'year'), (mult:3652.5; desc:'ten years'),
-     (mult:36525; desc:'century'), (mult:3625250; desc:'millenium'));
+     (mult:7; desc:'week'), (mult:30.436875; desc:'month'),
+     (mult:182.62125; desc:'six months'), (mult:365.2425; desc:'year'),
+     (mult:1826.2125; desc:'five years'), (mult:3652.425; desc:'ten years'),
+     (mult:36524.25; desc:'century'), (mult:365242.5; desc:'millenium'));
 
 type
   TFrmClimacogram = class(TForm)
     Chart: TChart;
+    lstSeries: TListBox;
+    MainMenu: TMainMenu;
+    Methods1: TMenuItem;
+    First1: TMenuItem;
+    Second1: TMenuItem;
+    Both1: TMenuItem;
+    Span1: TMenuItem;
+    mnuOneToTen: TMenuItem;
+    mnuOneToFive: TMenuItem;
+    mnuOneToTwo: TMenuItem;
+    Label1: TLabel;
+    File1: TMenuItem;
+    Edit1: TMenuItem;
+    mnuCopyChart: TMenuItem;
+    mnuCopyTabularData: TMenuItem;
+    mnuPrintChart: TMenuItem;
+    PrintDialog1: TPrintDialog;
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure lstSeriesClick(Sender: TObject);
+    procedure First1Click(Sender: TObject);
+    procedure mnuOneToTenClick(Sender: TObject);
+    procedure mnuPrintChartClick(Sender: TObject);
+    procedure mnuCopyChartClick(Sender: TObject);
   private
     FTimeseriesGrid: TTimeseriesGrid;
     procedure SetTimeseriesGrid(AValue: TTimeseriesGrid);
     procedure PrepareLineSeries(Timeseries: TTimeseries; Series: TLineSeries;
                                 all: Boolean);
     procedure AddLineSeries(Timeseries: TTimeseries; Chart: TChart;
-                                all: Boolean);
+                                all: Boolean; seriesNo: Integer);
+    procedure AddPeriodMarkers;
+    procedure FillList;
+    procedure DrawLines;
+    procedure Refresh;
   public
     property TimeseriesGrid: TTimeseriesGrid read FTimeseriesGrid
                                              write SetTimeseriesGrid;
@@ -60,13 +88,83 @@ end;
 
 procedure TFrmClimacogram.FormShow(Sender: TObject);
 begin
-  AddLineSeries(TimeseriesGrid.Data[0], Chart, False);
-  AddLineSeries(TimeseriesGrid.Data[0], Chart, True);
+  FillList;
+  Refresh;
+end;
+
+procedure TFrmClimacogram.Refresh;
+begin
+  DrawLines;
+end;
+
+procedure TFrmClimacogram.First1Click(Sender: TObject);
+begin
+  (Sender as TMenuItem).Checked := True;
+  Refresh;
+end;
+
+procedure TFrmClimacogram.lstSeriesClick(Sender: TObject);
+begin
+  Refresh;
+end;
+
+procedure TFrmClimacogram.mnuCopyChartClick(Sender: TObject);
+begin
+  Chart.CopyToClipboardMetafile(True);
+end;
+
+procedure TFrmClimacogram.mnuOneToTenClick(Sender: TObject);
+begin
+  (Sender as TMenuItem).Checked := True;
+  Refresh;
+end;
+
+procedure TFrmClimacogram.mnuPrintChartClick(Sender: TObject);
+begin
+  if PrintDialog1.Execute then
+    Chart.Print;
 end;
 
 procedure TFrmClimacogram.SetTimeseriesGrid(AValue: TTimeseriesGrid);
 begin
   FTimeseriesGrid := AValue;
+end;
+
+procedure TFrmClimacogram.DrawLines;
+var
+  i: Integer;
+  ACursor: TCursor;
+begin
+  ACursor := Screen.Cursor;
+  Chart.RemoveAllSeries;
+  try
+    Screen.Cursor := crHourGlass;
+    for i := 0 to lstSeries.Count - 1 do
+      if lstSeries.Selected[i] and
+                TTimeseries(lstSeries.Items.Objects[i]).TimeStepStrict and
+                (TTimeseries(lstSeries.Items.Objects[i]).Count>50) then
+      begin
+        if (First1.Checked) or (Both1.Checked) then
+          AddLineSeries(TTimeseries(lstSeries.Items.Objects[i]),
+                        Chart, False, i);
+        if (Second1.Checked) or (Both1.Checked) then
+          AddLineSeries(TTimeseries(lstSeries.Items.Objects[i]),
+                        Chart, True, i);
+      end;
+  finally
+    Screen.Cursor := ACursor;
+  end;
+    AddPeriodMarkers;
+end;
+
+procedure TFrmClimacogram.FillList;
+var
+  i: Integer;
+begin
+  for i := 0 to FTimeseriesGrid.Count-1 do
+    lstSeries.AddItem(IntToStr(i+1)+': '+FTimeseriesGrid.Data[i].Title,
+                            FTimeseriesGrid.Data[i]);
+  lstSeries.Selected[FTimeseriesGrid.ActiveIndex] := True;
 end;
 
 type
@@ -86,13 +184,22 @@ var
   i: Integer;
   fac: Integer;
   x, y, baseScale: Real;
+  MaxSpan: Integer;
 begin
+  if mnuOneToTen.Checked then
+    MaxSpan := 10
+  else if mnuOneToFive.Checked then
+    MaxSpan := 5
+  else if mnuOneToTwo.Checked then
+    MaxSpan := 2
+  else
+    Assert(False);
   scaleFunction := scaleFunctions[all];
-  maxScale := Timeseries.Count div 5;
+  maxScale := Timeseries.Count div MaxSpan;
   if Timeseries.TimeStep.LengthMinutes>0 then
     baseScale := Timeseries.TimeStep.LengthMinutes/1440
   else if Timeseries.TimeStep.LengthMonths>0 then
-    baseScale := Timeseries.TimeStep.LengthMonths*30.44
+    baseScale := Timeseries.TimeStep.LengthMonths*30.436875
   else
     Assert(False);
   i := 0;
@@ -101,7 +208,8 @@ begin
   begin
     x := baseScale * fac;
     y := scaleFunction(Timeseries, fac);
-    Series.AddXY(x, y);
+    if y>=0 then
+      Series.AddXY(x, y);
     Inc(i);
     if i<16 then
       fac := factors[i]
@@ -111,19 +219,65 @@ begin
 end;
 
 procedure TFrmClimacogram.AddLineSeries(Timeseries: TTimeseries; Chart: TChart;
-                                all: Boolean);
+                                all: Boolean; seriesNo: Integer);
 var
   ASeries: TLineSeries;
+  s: string;
 begin
   ASeries := nil;
   try
     ASeries := TLineSeries.Create(Chart);
+    s := Timeseries.Title;
+    if s='' then s := IntToStr(seriesNo+1)+':';
+    if all then s := s+' (multi pass)';
+    ASeries.Title := s;
+    ASeries.Tag := seriesNo;
     PrepareLineSeries(Timeseries, ASeries, all);
     Chart.AddSeries(ASeries);
     ASeries := nil;
   finally
     ASeries.Free;
   end;
+end;
+
+procedure TFrmClimacogram.AddPeriodMarkers;
+var
+  ASeries: TPointSeries;
+  i: Integer;
+  AMin, AMax: Real;
+begin
+  AMin := 1e37;
+  AMax := 0;
+  for i := 0 to Chart.SeriesCount - 1 do
+    with Chart.Series[i] do
+      if XValues.Count>0 then
+      begin
+        AMin := min(XValues.MinValue, AMin);
+        AMax := max(XValues.MaxValue, AMax)
+      end;
+  if AMin>AMax then
+    Exit;
+  ASeries := nil;
+  try
+    ASeries := TPointSeries.Create(Chart);
+    ASeries.Visible := True;
+    ASeries.ShowInLegend := False;
+    ASeries.Pointer.Visible := False;
+    Chart.Axes.Top.Maximum := 1e37;
+    Chart.Axes.Top.Minimum := 0.001;
+    Chart.Axes.Top.Maximum := AMax;
+    Chart.Axes.Top.Minimum := AMin;
+    ASeries.HorizAxis := aTopAxis;
+    for i := 0 to Length(scalePoints) - 1 do
+      with scalePoints[i] do
+        if (mult>=AMin) and (mult<=AMax) then
+          ASeries.AddXY(mult, 0, desc);
+    Chart.AddSeries(ASeries);
+    ASeries := nil;
+  finally
+    ASeries.Free;
+  end;
+
 end;
 
 end.
